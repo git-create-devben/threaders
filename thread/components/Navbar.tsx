452e624f63926c64da18f5f06d9491dc1b62622f
git-create-devben <DevBen.tech@outlook.com>
2023-11-13 16:17:22 +0100

    ?

--- thread/components/Navbar.tsx
+++ thread/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = (props: Props) => {
     <header className="flex justify-between p-2">
     <div className="flex items-center text-center gap-6 w-full mb-2">
       <h2 className="text-4xl font-bold mb-2">
-        <span className="text-blue-500">W</span>
+        <span className="text-blue-500">T</span>
         <span className="text-red-500">a</span>
         <span className="text-green-500">l</span>
         <span className="text-yellow-500">l</span>
